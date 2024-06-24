71d4ab11faaf894f06dd762d1dda1c058490147e
nabilasherif <nabila.sherif81@gmail.com>
2024-06-24 14:22:34 +0300

    env

--- envdir/env/env.go
+++ envdir/env/env.go
@@ -1,7 +1,14 @@
 package main
 
-// import //"os"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	//no imp
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		fmt.Println(pair[0])
+	}
 }
