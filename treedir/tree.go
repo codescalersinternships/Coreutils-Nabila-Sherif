dbe77c46bebd04bc520c3be5c174820685a123f2
nabilasherif <nabila.sherif81@gmail.com>
2024-06-24 15:33:36 +0300

    final inshallah

--- treedir/tree.go
+++ treedir/tree.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic((e))
+	}
+}
+func createEmptyFile(name string) {
+	d := []byte("")
+	check(os.WriteFile(name, d, 0644))
+}
 func main() {
-	//no impl
+	depth := os.Args[len(os.Args)-1]
+	i, e := strconv.Atoi(depth)
+	check(e)
+
+	err := os.Mkdir("subdir", 0755)
+	check(err)
+	defer os.RemoveAll("subdir")
+	os.MkdirAll("subdir/parent/child1", 0755)
+	os.MkdirAll("subdir/parent/child2", 0755)
+	os.MkdirAll("subdir/parent/child3", 0755)
+	//check(err)
+	createEmptyFile("subdir/parent/child1/file2")
+	createEmptyFile("subdir/parent/child2/file3")
+	createEmptyFile("subdir/parent/child3/file4")
+	c, err := os.ReadDir("subdir/parent")
+	check(err)
+	for _, entry := range c {
+		fmt.Println("|")
+		fmt.Println("――", entry.Name())
+		if i == 2 {
+			if entry.IsDir() {
+				d, err := os.ReadDir("subdir/parent/" + entry.Name())
+				check(err)
+				for _, ent := range d {
+					fmt.Println("    |")
+					fmt.Println("    ――", ent.Name())
+				}
+			}
+		}
+
+	}
 }
