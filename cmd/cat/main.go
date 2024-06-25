33e21aaa480c64e8257bc61526c3f99e13f116ee
nabilasherif <nabila.sherif81@gmail.com>
2024-06-25 14:05:33 +0300

    refactoring

--- cmd/cat/main.go
+++ cmd/cat/main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 func check(e error) {
 	if e != nil {
-		panic((e))
+		log.Fatal(e)
 	}
 }
 func main() {
 	if len(os.Args) > 1 {
-		arg := os.Args[1]
-		data, err := os.ReadFile(arg)
-		check(err)
-		fmt.Println(string(data))
-	} else { //log.fatal or os.exit(1)
+		var nvar int
+		file := os.Args[1]
+		printSpecificNum := false
+		flag.IntVar(&nvar, "n", 10, "an int var")
+		flag.Parse()
+		if strings.Contains(os.Args[1], "-n") {
+			file = os.Args[2]
+			printSpecificNum = true
+		}
+		data, e := os.ReadFile(file)
+		check(e)
+		s := strings.Split(string(data), "\n")
+		if printSpecificNum {
+			for j := 0; j < len(s) && j < nvar; j++ {
+				fmt.Println(s[j])
+			}
+		} else {
+			for _, str := range s {
+				fmt.Println((str))
+			}
+		}
+	} else {
 		os.Exit(1)
 	}
 }
--- cmd/tree/main.go
+++ cmd/tree/main.go
@@ -2,46 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 )
 
 func check(e error) {
 	if e != nil {
-		panic((e))
+		log.Fatal(e)
 	}
 }
-func createEmptyFile(name string) {
-	d := []byte("")
-	check(os.WriteFile(name, d, 0644))
-}
 func main() {
-	depth := os.Args[len(os.Args)-1]
+	depth := os.Args[2]
 	i, e := strconv.Atoi(depth)
 	check(e)
-
-	err := os.Mkdir("subdir", 0755)
-	check(err)
-	defer os.RemoveAll("subdir")
-	os.MkdirAll("subdir/parent/child1", 0755)
-	os.MkdirAll("subdir/parent/child2", 0755)
-	os.MkdirAll("subdir/parent/child3", 0755)
-	//check(err)
-	createEmptyFile("subdir/parent/child1/file2")
-	createEmptyFile("subdir/parent/child2/file3")
-	createEmptyFile("subdir/parent/child3/file4")
-	c, err := os.ReadDir("subdir/parent")
+	c, err := os.ReadDir(os.Args[len(os.Args)-1])
 	check(err)
 	for _, entry := range c {
 		fmt.Println("|")
 		fmt.Println("――", entry.Name())
 		if i == 2 {
 			if entry.IsDir() {
-				d, err := os.ReadDir("subdir/parent/" + entry.Name())
+				d, err := os.ReadDir(entry.Name())
 				check(err)
 				for _, ent := range d {
 					fmt.Println("    |")
-					fmt.Println("    ――", ent.Name())
+					fmt.Println("     ――", ent.Name())
 				}
 			}
 		}
